ac504bba85b9525da60d33066f740c18d1f663b5
FRONTLESS <35248965+Frontless@users.noreply.github.com>
2020-12-05 10:32:02 +0900

    no message

--- bg.js
+++ bg.js
@@ -3,16 +3,11 @@ const body = document.querySelector("body");
 const IMG_NUMBER = 5;
 
 function paintImage(imgNumber){
-    try{
-        const image = new Image();
-        image.src = `./VanillaJS/img/${imgNumber+1}.jpg`;
-        console.log(error, image.src);
-        image.classList.add('bgImage');
-        body.appendChild(image);
-    }
-    catch(error){
-        console.log(error, image.src);
-    }
+    const image = new Image();
+    image.src = `./img/${imgNumber+1}.jpg`;
+    console.log(error, image.src);
+    image.classList.add('bgImage');
+    body.appendChild(image);
 
 
     
